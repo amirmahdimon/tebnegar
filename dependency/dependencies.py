4f2f6647093bb63e7279d278458216cc2f24c40d
0ne-zero <pouria.khakpour9909@gmail.com>
2025-08-19 14:34:24 +0330

    Impl: admin api key authentication

--- dependency/dependencies.py
+++ dependency/dependencies.py
@@ -1,3 +1,21 @@
 from db.session import get_db
+from fastapi import  HTTPException, Security
+from fastapi.security import APIKeyHeader
+from starlette import status
+from config.settings import SETTINGS
 
+# Define the API Key security scheme
+api_key_header = APIKeyHeader(name="X-API-KEY", auto_error=False)
 
+async def get_admin_api_key(api_key: str = Security(api_key_header)):
+    """
+    Dependency to validate the admin API key.
+    In a real app, use a secure comparison method and load keys from a vault.
+    """
+    if api_key == SETTINGS.ADMIN_API_KEY:
+        return api_key
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Could not validate credentials",
+        )
