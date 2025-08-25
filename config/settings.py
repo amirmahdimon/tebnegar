8df7e58ae9dbf4d0856dba203356db792c1ebfb5
0ne-zero <pouria.khakpour9909@gmail.com>
2025-08-25 06:36:25 +0330

    Impl: google oatuh & jwt related config fields

--- config/settings.py
+++ config/settings.py
@@ -8,6 +8,16 @@ class Settings(BaseSettings):
     HTTPS_PROXY: str | None = None
     DEVELOPMENT: bool
 
+    # Google OAuth
+    GOOGLE_CLIENT_ID: str
+    GOOGLE_CLIENT_SECRET: str
+    GOOGLE_REDIRECT_URI: str
+
+    # JWT
+    SECRET_KEY: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8  # 8 days
+    ALGORITHM: str = "HS256"
+
     class Config:
         env_file = ".env"
 
