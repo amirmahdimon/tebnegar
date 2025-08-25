33dfe10b5f2c8f2d3d290aa35ab86ac36bcbab81
0ne-zero <pouria.khakpour9909@gmail.com>
2025-08-25 06:35:17 +0330

    Impl: user table in database

---
+++ db/model/user.py
@@ -0,0 +1,15 @@
+import uuid
+from sqlalchemy import Column, String, DateTime, func
+from sqlalchemy.dialects.postgresql import UUID
+from db.base import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    google_id = Column(String, unique=True, index=True, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    full_name = Column(String, nullable=True)
+    picture_url = Column(String, nullable=True)
+    created_at = Column(DateTime, default=func.now())
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
